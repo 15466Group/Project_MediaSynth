fd83ae1a0a1bbfa3f55522ce9f83b9b1f383ff87
Justice Frimpong <jfrimpon@andrew.cmu.edu>
2015-10-23 17:32:55 -0400

    proj

---
+++ EbbingHausGalaxy/Assets/Scripts/CameraPanning.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPanning : MonoBehaviour {
+
+	private Vector3 velocity;
+	public Vector3 endPos;
+	private float runTime;
+	private float totalTime;
+
+	// Use this for initialization
+	void Start () {
+		runTime = 5.0f;
+		velocity = Vector3.zero;
+		velocity.x = (Vector3.Distance(endPos, transform.position))/runTime;
+		totalTime = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		totalTime += Time.deltaTime;
+		if (totalTime < runTime) {
+			transform.position += velocity * Time.deltaTime;
+//			Debug.Log (totalTime);
+		}
+	}
+}
---
+++ EbbingHausGalaxy/Assets/Scripts/PlanetRotations.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PlanetRotations : MonoBehaviour {
+
+	private Quaternion rot;
+	private float smooth;
+
+	// Use this for initialization
+	void Start () {
+		rot = Quaternion.identity;
+		smooth = 200.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (Transform orbiting in transform) {
+			orbiting.RotateAround(orbiting.position, orbiting.up, smooth * Time.deltaTime);
+		}
+	}
+}
